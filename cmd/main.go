94f8b8586232c313061a7196cb3365a22b2339a3
Buk Bukowski <38087302+bukowa@users.noreply.github.com>
2020-09-13 21:16:05 +0200

    add as cobra cmd

--- cmd/main.go
+++ cmd/main.go
@@ -1,22 +1,45 @@
 package main
 
 import (
+	"github.com/bukforks/cobra"
 	. "github.com/bukowa/gorelease"
 	"log"
+	"os"
 )
 
-var Version string
+var (
+	Version string
+	Path    string
+)
 
-func main() {
-	log.Printf("hello world from gorelease%s", Version)
-	release := FromFile(".gorelease.yaml")
-	handler := NewHandler()
-	if err := handler.Prepare(release); err != nil {
-		log.Fatal(err)
-	}
-	for _, t := range release.Targets {
-		if err := handler.Build(&t); err != nil {
+var Command = &cobra.Command{
+	Use:     "gorelease",
+	Short:   "build and release your go application.",
+	Version: Version,
+}
+
+var Build = &cobra.Command{
+	Use:     "build",
+	Short:   "go build targets",
+	Version: Version,
+	Run: func(cmd *cobra.Command, args []string) {
+		release := FromFile(Path)
+		handler := NewHandler()
+		if err := handler.Prepare(release); err != nil {
 			log.Fatal(err)
 		}
+		for _, t := range release.Targets {
+			if err := handler.Build(&t); err != nil {
+				log.Fatal(err)
+			}
+		}
+	},
+}
+
+func main() {
+	Command.AddCommand(Build)
+	Build.Flags().StringVarP(&Path, "config", "c", ".gorelease.yaml", "path go gorelease config file")
+	if err := Command.Execute(); err != nil {
+		os.Exit(1)
 	}
 }
--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/bukowa/gorelease
 go 1.15
 
 require (
+	github.com/bukforks/cobra v1.0.4
 	github.com/pkg/errors v0.9.1
 	gopkg.in/yaml.v2 v2.3.0
 )
